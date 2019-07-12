b49170251886268f4b96f15989c30cf9fc048a25
ML <matthew.lavis@gmail.com>
2019-07-12 19:22:45 +0100

    Removed more output files

--- tariff-reference/fta_schedules safe/create_fta.py
+++
@@ -1,9 +0,0 @@
-# Import standard modules
-import sys
-import glob as g
-import functions as f
-
-# Set up
-app = g.app
-app.create_document()
-app.shutDown()
--- tariff-reference/suspensions/glob.py
+++
@@ -1,5 +0,0 @@
-# Import custom modules
-from application import application
-
-app = application()
-
